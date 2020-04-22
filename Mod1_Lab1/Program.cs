14610b1559136346a71332e074bbc79b2715869b
chisoftltd <chisoftltd@gmail.com>
2020-04-22 16:26:55 +0200

    MODULE 3 TUTORIAL LAB 1 - CREATING AND USING METHODS

--- Mod1_Lab1/Program.cs
+++ Mod1_Lab1/Program.cs
@@ -13,7 +13,14 @@ namespace Mod1_Lab1
 
             try
             {
-                Sum(20, 40);
+                int result = Sum(20, 40);
+                Console.WriteLine($"Calling Sum() with two arguments, result is: {result}");
+
+                int result3 = Sum(10, 50, 80);
+                Console.WriteLine($"Calling Sum() with three arguments, result is: {result3}");
+
+                double dblResult = Sum(20.5, 30.6);
+                Console.WriteLine($"Calling Sum() with two doubles, result in: {dblResult}");
             }
             finally
             {
@@ -23,10 +30,22 @@ namespace Mod1_Lab1
 
         }
 
-        static void Sum(int first, int second)
+        static int Sum(int first, int second)
         {
             int sum = first + second;
-            Console.WriteLine($"The sum of {first} and {second} is: {sum}");
+            return sum;
+        }
+
+        static int Sum(int first, int second, int third)
+        {
+            int sum = first + second + third;
+            return sum;
+        }
+
+        static double Sum(double first, double second)
+        {
+            double result = first + second;
+            return result;
         }
     }
 }
