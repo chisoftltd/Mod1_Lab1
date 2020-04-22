fa2916fb5c2602983eb13f7bb5e5f7e4eacdc222
chisoftltd <chisoftltd@gmail.com>
2020-04-22 15:15:05 +0200

    TUTORIAL LAB 3 FOR MODULE 2 - USING A FOR LOOP

--- Mod1_Lab1/Program.cs
+++ Mod1_Lab1/Program.cs
@@ -19,6 +19,16 @@ namespace Mod1_Lab1
                     Console.WriteLine($"Twice the counter is : {counter * 2}");
                 }
 
+                int outer;
+                int inner;
+
+                for(outer = 2; outer < 100; outer++)
+                {
+                    for (inner = 2; inner <= (outer / inner); inner++)
+                        if ((outer % inner) == 0) break;
+                    if (inner > (outer / inner))
+                        Console.WriteLine("{0} is prime", outer);
+                }
             }
             finally
             {
